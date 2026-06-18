19f4700d2608daa7128d3abd3dd3a84ce408d0bb
kinngddx <ddxboss70@gmail.com>
2026-06-18 09:53:05 +0530

    Added vercel link

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "https://your-vercel-app.vercel.app"  
+      "https://vyorius-drones-private-limited-2fvmsgms5-kinngddxs-projects.vercel.app/"  
     ],
     methods: ["GET", "POST"]
   }
